df97b51b06887fe9d1c5b187d1ae484f2eab2962
SeungminAhn <smahn9123@gmail.com>
2024-12-29 11:56:09 +0900

    monthly_challenges: use variable interpolation
    For dynamically injecting arguments into template file.
    
    Signed-off-by: SeungminAhn <smahn9123@gmail.com>

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -50,6 +50,13 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(
+            request,
+            "challenges/challenge.html",
+            {
+                "text": challenge_text,
+                "month_name": month.capitalize(),
+            },
+        )
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
